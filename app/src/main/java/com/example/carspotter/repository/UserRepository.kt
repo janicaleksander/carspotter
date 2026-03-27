3dcbd29287f5e81552aa255451c448761dc5e3c4
Aleksander <141730118+janicaleksander@users.noreply.github.com>
2026-03-27 22:54:26 +0100

    fix: repair user synchronization

--- app/src/main/java/com/example/carspotter/repository/UserRepository.kt
+++ app/src/main/java/com/example/carspotter/repository/UserRepository.kt
@@ -3,6 +3,7 @@ package com.example.carspotter.repository
 import com.example.carspotter.BuildConfig
 import com.example.carspotter.dao.UserDao
 import com.example.carspotter.models.User
+import io.appwrite.Query
 import io.appwrite.services.TablesDB
 import java.time.LocalDateTime
 import javax.inject.Inject
@@ -13,21 +14,30 @@ class UserRepository @Inject constructor(
 ) {
     //get users itd
 
+    //TODO so sync func are going to throw exception if there is not result, when the result are mandatory
+    //other function are going to return nullabe? or lists
 
     suspend fun syncUser(userId: String){
         try {
-            val userResponse = tablesDB.getRow(
+            val userResponse = tablesDB.listRows(
                 databaseId = BuildConfig.DATABASE_ID,
                 tableId = "user",
-                rowId = userId
+                queries = listOf(
+                    Query.equal("\$id", userId),
+                    Query.limit(1)
+                )
             )
-
-            val user = User(
-                id = userId,
-                nickname = userResponse.data["nickname"] as String,
-                createdAt = LocalDateTime.parse(userResponse.data["createdAt"] as String)
-            )
-            userDao.insert(user)
+            if (userResponse.rows.isEmpty())    {
+                throw Exception("Cant synchronize - user not found")
+            }
+            val user = userResponse.rows.map { row ->
+                User(
+                    id = userId,
+                    nickname = row.data["nickname"] as String,
+                    createdAt = LocalDateTime.parse(row.data["createdAt"] as String)
+                )
+            }
+            userDao.insert(user.first())
         }catch (e: Exception){
             throw e
         }
