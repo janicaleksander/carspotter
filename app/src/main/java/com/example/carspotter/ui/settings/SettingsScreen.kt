23c89104f46b1124846d038321a261505210ca2f
Aleksander <141730118+janicaleksander@users.noreply.github.com>
2026-04-29 15:01:22 +0200

    feat: add text field common

--- app/src/main/java/com/example/carspotter/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/carspotter/ui/settings/SettingsScreen.kt
@@ -1,8 +1,11 @@
 package com.example.carspotter.ui.settings
 
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.carspotter.viewmodels.AuthViewModel
 import com.example.carspotter.viewmodels.SettingsViewModel
@@ -13,13 +16,18 @@ import com.example.carspotter.viewmodels.SettingsViewModel
 fun SettingsScreen(
     viewModel: SettingsViewModel,
     authViewModel: AuthViewModel) {
+    val context = LocalContext.current
     val settings by viewModel.settings.collectAsStateWithLifecycle()
     val randomPhoto by viewModel.random_photo.collectAsStateWithLifecycle()
 
 
     SettingsContent(
         settings = settings,
-        onPermissionsClick = { /* TODO */ },
+        onPermissionsClick = {
+            val packageUri = Uri.fromParts("package", context.packageName, null)
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, packageUri)
+            context.startActivity(intent)
+        },
         onLogoutClick = { authViewModel.logout() },
         imageUrl = randomPhoto ?: "https://via.placeholder.com/150"
     )
